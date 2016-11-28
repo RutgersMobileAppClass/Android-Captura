82c77176a09413b1ea12636f28fc368b2d43ba70
Neil Patel <neil.m.patel@rutgers.edu>
2016-11-27 22:23:48 -0500

    Removed conflict lines

--- app/src/main/java/finalproject/mobileappclass/com/captura/MainActivity.java
+++ app/src/main/java/finalproject/mobileappclass/com/captura/MainActivity.java
@@ -12,10 +12,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.Toast;
-=======
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
