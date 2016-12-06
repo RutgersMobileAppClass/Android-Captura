7cf6f74ae4af5071edb96ab428557c0c681e4716
Sahishnu <bhoolku5443@gmail.com>
2016-12-06 15:20:51 -0500

    Bottom Nav + Actionbar
    Added bottom nav.
    Added action bar icons for all activities.
    Bottom nav launches new activities.

--- app/src/main/java/finalproject/mobileappclass/com/captura/HistoryActivity.java
+++ app/src/main/java/finalproject/mobileappclass/com/captura/HistoryActivity.java
@@ -1,9 +1,17 @@
 package finalproject.mobileappclass.com.captura;
 
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import java.util.ArrayList;
 
@@ -24,6 +32,50 @@ public class HistoryActivity extends AppCompatActivity {
         historyOfRequests = (ListView) findViewById(R.id.HistoryListView);
         adapter = new HistoryAdapter(this, requests);
         historyOfRequests.setAdapter(adapter);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTab(R.id.tab_history);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_photo) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+
+                if (tabId == R.id.tab_quizzes) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+
+                    Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_historyactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_settings:
+
+                Toast.makeText(HistoryActivity.this, "Settings!", Toast.LENGTH_SHORT).show();
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private class DBLoadTask extends AsyncTask<Void, Void, ArrayList<TranslationRequest>> {
--- app/src/main/java/finalproject/mobileappclass/com/captura/QuizActivity.java
+++ app/src/main/java/finalproject/mobileappclass/com/captura/QuizActivity.java
@@ -1,7 +1,17 @@
 package finalproject.mobileappclass.com.captura;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +19,54 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTab(R.id.tab_quizzes);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_history) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+                    startActivity(intent);
+                }
+
+                if (tabId == R.id.tab_photo) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_quizactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_newQuiz:
+
+                Toast.makeText(QuizActivity.this, "New Quiz!", Toast.LENGTH_SHORT).show();
+
+            case R.id.action_settings:
+
+                Toast.makeText(QuizActivity.this, "Settings!", Toast.LENGTH_SHORT).show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
