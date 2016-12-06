8f80fd12201d24ba5148131af7d15c3d0e4653f6
Neil Patel <neil.m.patel@rutgers.edu>
2016-12-06 14:19:15 -0500

    Added quiz history activity layout files

--- app/src/main/java/finalproject/mobileappclass/com/captura/QuizActivity.java
+++ app/src/main/java/finalproject/mobileappclass/com/captura/QuizActivity.java
@@ -1,8 +1,13 @@
 package finalproject.mobileappclass.com.captura;
 
-/**
- * Created by viral on 12/6/16.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class QuizActivity {
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
 }
